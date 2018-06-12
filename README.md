e4f48d86467a927330ec40c5ef58f396e64b9c1c
Jared Bents <jared.bents@rockwellcollins.com>
2018-06-12 09:43:47 -0500

    INIT: Update to include initial items
    update to include initial items
    
    Signed-off-by: Jared Bents <jared.bents@rockwellcollins.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a collection of stuff I use on multiple machines for ease of setup and version control
---
+++ tmux_stuff/tmux-init.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Window 0
+tmux rename-window alderaan
+tmux split-window -h
+tmux split-window
+
+# Window 1
+tmux new-window
+tmux select-window -t 1
+tmux rename-window tatooine
+tmux split-window -h
+tmux split-window
+
+# Window 2
+tmux new-window
+tmux select-window -t 2
+tmux rename-window hoth
+tmux split-window -h
+
+# Window 3
+tmux new-window
+tmux select-window -t 3
+tmux rename-window mandelore
+
+# Window 4
+tmux new-window
+tmux select-window -t 4
+tmux rename-window kashyyyk
+
+# Window 5
+tmux new-window
+tmux select-window -t 5
+tmux rename-window jakku
+
+# Window 6
+tmux new-window
+tmux select-window -t 6
+tmux rename-window dagobah
+
+# Window 7
+tmux new-window
+tmux select-window -t 7
+tmux rename-window jedha
+
+# Return to window 0
+tmux select-window -t 0
